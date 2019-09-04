0a91769f7d16356dd8d14a783e131f6831aaf449
Gonzalo Montero <gma.montero@gmail.com>
2019-09-04 17:18:38 -0300

    Create version script

---
+++ createVersion.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+rm -rf benchmarks meteor scripts tasks templates typing-tests
+rm -rf Moment.js.nuspec package-lock.json Gruntfile.js bower.json composer.json component.json
+rm -rf .editorconfig .spmignore .gitattributes .npmignore .travis.yml .jscs.json
+
